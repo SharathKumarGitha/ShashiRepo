ad783169021ce891696c33d75769285103ed2568
sharathgitha627 <sharathgitha1627@gmail.com>
2024-06-15 22:57:47 +0530

    one line is added in central repo

--- src/main/java/com/springbootdemoonehour/SpringBootDemoOneHourApplication.java
+++ src/main/java/com/springbootdemoonehour/SpringBootDemoOneHourApplication.java
@@ -8,6 +8,9 @@ public class SpringBootDemoOneHourApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoOneHourApplication.class, args);
+		
+		System.out.println("this is shashi");
+		System.out.println("this is sharath");
 	}
 
 }
