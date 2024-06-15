5b0a50b89dadab748b467539a61059ff3588752a
sharathgitha627 <sharathgitha1627@gmail.com>
2024-06-15 22:39:22 +0530

    first commit

---
+++ src/main/java/com/springbootdemoonehour/SpringBootDemoOneHourApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdemoonehour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoOneHourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoOneHourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootdemoonehour/SpringBootDemoOneHourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdemoonehour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoOneHourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
